7e142c8533d96cc9837763e51301748295c999c4
Jorge Montesinos <osmarmontes50@gmail.com>
2023-06-28 11:15:16 -0500

    added js and html

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,74 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    // console.log("words", text.split(" ").length);
+    return text.split(" ").length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let textWithoutSpaces = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] !== " " && text[i] !== "." && text[i] !== ",") {
+        textWithoutSpaces += text[i];
+      }
+    }
+    return textWithoutSpaces.length;
+    // return text.replace(/[\s\.,]/g, "").length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ");
+    const cantWords = words.length;
+    const sumLength = words.reduce((acc, word) => {
+      return acc + word.length;
+    }, 0);
+    const average = sumLength / cantWords;
+    return Math.round(average * 100) / 100;
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    // const numbers = text.match(/\d/g);
+    // console.log({numbers});
+
+    const words = text.split(" ");
+    const arrNumbers = [];
+    // let arrNumbers = "";
+    for (let i = 0; i < words.length; i++) {
+      const isNumber = words[i] * 1;
+      if (!isNaN(isNumber)) {
+        arrNumbers.push(words[i]);
+        // arrNumbers += words[i];
+      }
+    }
+    /*
+    const arrNumbers = words.filter((word) => {
+      const isNumber = word * 1;
+      return !isNaN(isNumber);
+      // return !isNaN(word);
+    });
+    */
+    return arrNumbers.length;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    // const numbers = text.match(/\d/g);
+    // return numbers ? numbers.reduce((acc, number) => {
+    //   return acc + parseInt(number);
+    // }, 0) : 0;
+    const words = text.split(" ");
+    let sum = 0;
+
+    for (let i = 0; i < words.length; i++) {
+      const isNumber = words[i] * 1;
+      if (!isNaN(isNumber)) {
+        sum += isNumber;
+      }
+    }
+    return sum;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
+
+const {
+  getWordCount,
+  getCharacterCount,
+  getCharacterCountExcludingSpaces,
+  getAverageWordLength,
+  getNumberCount,
+  getNumberSum,
+} = analyzer;
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const liCharacters = document.querySelector("ul li:nth-child(1)");
+const liCharactersNoSpaces = document.querySelector("ul li:nth-child(2)");
+const liWords = document.querySelector("ul li:nth-child(3)");
+const liNumbers = document.querySelector("ul li:nth-child(4)");
+const liSumNumbers = document.querySelector("ul li:nth-child(5)");
+const liAverageLength = document.querySelector("ul li:nth-child(6)");
+const textArea = document.querySelector("textarea");
+const btnReset = document.getElementById("reset-button");
+
+const updateResults = (e) => {
+  const text = e.target.value;
+  liCharacters.innerText = `Caracteres ${getCharacterCount(text)}`;
+  liCharactersNoSpaces.innerText = `Caracteres sin espacios ${getCharacterCountExcludingSpaces(text)}`;
+  liWords.innerText = `Palabras ${getWordCount(text)}`;
+  liNumbers.innerText = `Números ${getNumberCount(text)}`;
+  liSumNumbers.innerText = `Suma números ${getNumberSum(text)}`;
+  liAverageLength.innerText = `Promedio longitud ${getAverageWordLength(text)}`;
+};
+
+const resetTextArea = () => {
+  textArea.value = "";
+  updateResults({ target: { value: "" } });
+};
+
+textArea.addEventListener("keyup", updateResults);
+btnReset.addEventListener("click", resetTextArea);
